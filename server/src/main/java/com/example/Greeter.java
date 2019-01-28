e878b2a6803cee4817b991d96d9312db5f120dcf
TaniaRodrigues <47112683+TaniaRodrigues@users.noreply.github.com>
2019-01-28 16:46:38 +0100

    rectification greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,6 +9,6 @@ public class Greeter {
   }
 
   public final String greet(final String someone) {
-    return String.format("Hello, Tania, someone");
+    return String.format("Hello tania, %s!", someone);
   }
 }
